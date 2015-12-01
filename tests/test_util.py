5133446c5ea7a38b6f4b64eb186cc3bde728b742
Bryce Mecum <petridish@gmail.com>
2015-11-30 16:57:59 -0900

    Fix typo on util test

--- tests/test_util.py
+++ tests/test_util.py
@@ -12,7 +12,7 @@ def test_can_load_a_file_into_a_model():
 
     assert isinstance(parsed, RDF.Model)
 
-def test_cant_load_a_file_into_a_model_of_the_wrong_foramt():
+def test_cant_load_a_file_into_a_model_of_the_wrong_format():
     parsed = util.load_file_into_model("void-dataset.ttl")
 
     assert parsed is None
