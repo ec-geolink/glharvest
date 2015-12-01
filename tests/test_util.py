e7868544e95a83333e848fd25bcc92c12b9cb610
Bryce Mecum <petridish@gmail.com>
2015-11-30 16:57:51 -0900

    Add string-to-model tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -16,3 +16,37 @@ def test_cant_load_a_file_into_a_model_of_the_wrong_foramt():
     parsed = util.load_file_into_model("void-dataset.ttl")
 
     assert parsed is None
+
+def test_can_load_a_string_into_a_model():
+	turtle = """
+	@base <http://example.org/> .
+	@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+	@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+	@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+	@prefix rel: <http://www.perceive.net/schemas/relationship/> .
+
+	<#green-goblin>
+	    rel:enemyOf <#spiderman> ;
+	    a foaf:Person ;    # in the context of the Marvel universe
+	    foaf:name "Green Goblin" .
+
+	<#spiderman>
+	    rel:enemyOf <#green-goblin> ;
+	    a foaf:Person ;
+	    foaf:name "Spiderman" .
+	"""
+
+
+	parsed = util.load_string_into_model(turtle, format="turtle")
+
+	assert parsed is not None
+
+def test_cant_load_a_bogus_string_into_a_model():
+	turtle = """
+	bogus ttl content
+	"""
+
+
+	parsed = util.load_string_into_model(turtle, format="turtle")
+
+	assert parsed is None
