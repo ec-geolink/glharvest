49a185748598bc4e32a0b6fa7fb023af62fbe7d1
Bryce Mecum <petridish@gmail.com>
2015-11-30 16:58:29 -0900

    Add a basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='glharvest',
+      version='0.1',
+      description='A library supporting the GeoLink Harvest System.',
+      author='Bryce Mecum',
+      author_email='mecum@nceas.ucsb.edu',
+      url='https://github.com/ec-geolink/glharvest',
+      packages=['glharvest']
+     )
