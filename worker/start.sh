8c5ad02269a37ff90a40d01da19dc741007ba738
Bryce Mecum <petridish@gmail.com>
2015-12-04 13:31:33 -0900

    Make worker and scheduler wait for graphdb

---
+++ worker/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Wait for Sesame
+echo "Waiting for Sesame"
+until $(curl --output /dev/null --silent --head --fail http://graphdb:8080/openrdf-workbench/repositories/NONE/repositories); do
+    echo '.'
+    sleep 2
+done
+
+# Start the Worker
+echo "Starting Worker"
+python -u work.py
