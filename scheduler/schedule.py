2c375406c11e724d9c62ee9cb38127de7f5c120d
Bryce Mecum <mecum@mecum.local>
2015-12-16 10:23:36 -0900

    Clean up jobs file

--- scheduler/schedule.py
+++ scheduler/schedule.py
@@ -16,23 +16,23 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def queue_status_job():
-    q.enqueue('glharvest.jobs.status')
+    q.enqueue(glharvest.jobs.status)
 
-@sched.scheduled_job('interval', hours=1)
+@sched.scheduled_job('interval', minutes=1)
 def queue_update_job():
-    q.enqueue('glharvest.jobs.update', timeout=600) # Timeout of 10min
+    q.enqueue(glharvest.jobs.update, timeout=600) # Timeout of 10min
 
 @sched.scheduled_job('interval', hours=1)
 def queue_export_job():
-    q.enqueue('glharvest.jobs.export')
+    q.enqueue(glharvest.jobs.export)
 
-# Queue the status job syncrhonously so the test repo is created
-# This is a workaround to a bug in Sesame that lets you create two repositories
-# with the same ID which puts the database into an unusable state
 
+# Wait a bit for Sesame to start
 time.sleep(10)
-q.enqueue('glharvest.jobs.status')
-q.enqueue('glharvest.jobs.update')
+
+# Queue the status job first. This creates the repository before any other
+# jobs are run.
+q.enqueue(glharvest.jobs.status)
 
 # Start the scheduler
 sched.start()
