2983e6f9a025bd25be1d2a4329eeed831f7b318c
Bryce Mecum <mecum@mecum.local>
2015-12-16 16:42:33 -0900

    Fix references in scheduler

--- scheduler/schedule.py
+++ scheduler/schedule.py
@@ -16,15 +16,15 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def queue_status_job():
-    q.enqueue(glharvest.jobs.status)
+    q.enqueue(jobs.status)
 
 @sched.scheduled_job('interval', minutes=1)
 def queue_update_job():
-    q.enqueue(glharvest.jobs.update, timeout=600) # Timeout of 10min
+    q.enqueue(jobs.update, timeout=600) # Timeout of 10min
 
 @sched.scheduled_job('interval', hours=1)
 def queue_export_job():
-    q.enqueue(glharvest.jobs.export)
+    q.enqueue(jobs.export)
 
 @sched.scheduled_job('interval', minutes=1)
 def print_jobs_job():
@@ -35,7 +35,7 @@ time.sleep(10)
 
 # Queue the status job first. This creates the repository before any other
 # jobs are run.
-q.enqueue(glharvest.jobs.status)
+q.enqueue(jobs.status)
 
 # Start the scheduler
 sched.start()
