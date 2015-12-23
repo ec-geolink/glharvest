e9ebe73a60e767c6b4a85477454c89699e18e65e
Bryce Mecum <mecum@mecum.local>
2015-12-23 14:43:13 -0900

    Replace print debugging with logging and raise Exceptions when we oughta

--- glharvest/glharvest/jobs.py
+++ glharvest/glharvest/jobs.py
@@ -10,6 +10,7 @@ import urllib
 import datetime
 from dateutil.parser import parse
 import RDF
+import logging
 
 from d1lod.sesame import Store, Repository
 from glharvest import registry, void, util
@@ -32,8 +33,8 @@ def update():
     Reads the registry file and does work depending on what it finds.
     """
 
-    print "job.update()"
-    print "Parsing registry file."
+    JOB_NAME = "UPDATE"
+    logging.info("[%s] Job started.", JOB_NAME)
 
     # Establish the location of the registry file
     # The else case will run when not running in a container
@@ -42,9 +43,10 @@ def update():
     else:
         registry_filepath = 'registry.yml'
 
+    logging.info("[%s] Loading registry file from location '%s'.", JOB_NAME, registry_filepath)
+
     if not os.path.isfile:
-        print "Couldn't locate the registry file at the provided path: %s. Exiting." % registry_filepath
-        return
+        raise Exception("Couldn't locate the registry file at the provided path: %s. Exiting.", registry_filepath)
 
     registry_file = registry.parse_registry_file(registry_filepath)
 
@@ -52,30 +54,27 @@ def update():
         print "Failed to parse registry file. Exiting."
         return
 
-    print "Registry file parsed. %d provider(s) found." % len(registry_file)
+    logging.info("[%s] Registry file parsed. %d provider(s) found.", JOB_NAME, len(registry_file))
 
     for provider in registry_file:
-        # Validate and extract information for each provider
-        print "Validating provider: %s." % provider
+        logging.info("[%s] Processing provider '%s'.", JOB_NAME, provider)
 
         # Check for VoID
         if 'void' not in registry_file[provider]:
-            print "Location of VoID dataset not found for this provider in the registry. Skipping."
+            logging.error("[%s] Location of VoID dataset not found for this provider in the registry. Skipping.", JOB_NAME)
             continue
 
         voidfile = registry_file[provider]['void']
 
         # Check for modified value
         if 'modified' not in registry_file[provider]:
-            print "Location of VoID dataset not found for this provider in the registry."
+            logging.info("[%s] Modified date(time) was not found in the registry for provider '%s'. This will force harvesting for this provider.", JOB_NAME, provider)
             registry_modified = None
         else:
             registry_modified = registry_file[provider]['modified']
-            print registry_modified
             registry_modified = parse(str(registry_modified), ignoretz=True)
 
-            print registry_modified
-        print "Registry has a last modified value of %s." % registry_modified
+        logging.info("[%s] Provider '%s' was last modified at '%s'.", JOB_NAME, provider, registry_modified)
 
         # Get and parse the VoID file
         print "Attemting to retrieve VoID file from location %s." % voidfile
@@ -83,7 +82,6 @@ def update():
         try:
             r = requests.get(voidfile)
         except:
-            print "Failed to get voidfile located at `%s`." % voidfile
             continue
 
         void_string_format = "rdfxml"
@@ -96,17 +94,15 @@ def update():
         model = util.load_string_into_model(r.text, fmt=void_string_format)
         void_model = void.parse_void_model(model)
 
-        print void_model
 
         for provider_dataset in void_model:
-            print "Processing provider VoID:Dataset %s." % provider_dataset
 
             if 'modified' not in void_model[provider_dataset]:
-                print "VoID:Dataset found in VoID dump did not have dcterms:modified value. Skipping."
+                logging.error("[%s] VoID:Dataset found in VoID dump did not have dcterms:modified value. Skipping.", JOB_NAME)
                 continue
 
             if 'features' not in void_model[provider_dataset]:
-                print "VoID:Dataset has no feature declaration. Skipping."
+                logging.error("[%s] VoID:Dataset has no feature declaration. Skipping.", JOB_NAME)
                 continue
 
             modified = void_model[provider_dataset]['modified']
@@ -114,14 +110,11 @@ def update():
             try:
                 modified = parse(modified, ignoretz=True)
             except:
-                print "Failed to parse modified time string of %s. Skipping." % modified
                 continue
 
-            print "After parsing, `modified` value is %s." % modified
 
             # TODO process features
             features = void_model[provider_dataset]['features']
-            print "Found features: %s" % features
 
             print "Updating dataset if %s is greater than %s." % (modified, registry_modified)
 
@@ -130,78 +123,76 @@ def update():
 
                 # Set up repository connection
                 s = Store(SESAME_HOST, SESAME_PORT)
-                r = Repository(s, SESAME_REPOSITORY)
+            if registry_modified is not None and modified <= registry_modified:
+                logging.info("[%s] Provider '%s' has not been updated since last update. Continuing on to next provider in registry.", JOB_NAME, provider)
 
-                # Just delete all triples in the context
-                print "Deleting triples in context %s." % provider
-                r.delete_triples_about('?s', context=provider)
+            # Just delete all triples in the context
+            logging.info("[%s] Deleting triples in context %s.", JOB_NAME, provider)
+            repository.delete_triples_about('?s', context=provider)
 
-                data_dumps = void_model[provider_dataset]['dumps']
 
-                for dump in data_dumps:
-                    print "Processing %s %s %s." % (provider, provider_dataset, dump)
+            for dump in data_dumps:
 
-                    # Create a temporary name for the file
-                    outfilename = datetime.datetime.now().strftime("%s-%f")
+                # Create a temporary name for the file
 
-                    try:
-                        # resp = requests.get(dump)
-                        urllib.urlretrieve(dump, outfilename)
-                    except:
-                        print "Failed to fetch the void file at `%s`. Skipping." % dump
-                        continue
+                try:
+                    urllib.urlretrieve(dump, outfilename)
+                except:
+                    logging.error("[%s] Failed to fetch the void file at '%s'. Skipping dump file.", JOB_NAME, dump)
 
-                    # Decide the format (from looking at the URL string)
-                    dump_file_format = "rdfxml"
+                # Decide the format (from looking at the URL string)
 
-                    # Use another format is we detect a different one
-                    if dump.endswith('ttl'):
-                        dump_file_format = 'turtle'
+                # Use another format is we detect a different one
+                if dump.endswith('ttl'):
 
-                    # parser = RDF.Parser(name=dump_file_format)
 
-                    # Delete triples about each subject (streaming)
-                    # for statement in parser.parse_as_stream('file:' + outfilename):
-                    #     # Don't delete statements about non_URI subjects because
-                    #     # we can't
-                    #     if not statement.subject.is_resource():
-                    #         continue
-                    #
-                    #     print "Deleting triples in context %s about %s." % (provider, str(statement.subject))
-                    #     r.delete_triples_about(statement.subject, context=provider)
+                # Delete triples about each subject (streaming)
+                # for statement in parser.parse_as_stream('file:' + outfilename):
+                #     # Don't delete statements about non_URI subjects because
+                #     # we can't
+                #     if not statement.subject.is_resource():
+                #         continue
+                #
+                #     print "Deleting triples in context %s about %s." % (provider, str(statement.subject))
 
-                    # Import the file
-                    print "Importing temp file '%s' into named graph '%s'." % (outfilename, provider)
-                    r.import_from_file(outfilename, context=provider, fmt=dump_file_format)
+                # Import the file
+                logging.info("[%s] Importing temp file '%s' into named graph '%s'.", JOB_NAME, outfilename, provider)
 
-                    # Delete the temp file
+                # Delete the temp file
                     os.remove(outfilename)
-
+                except:
 
                 # Update registry file on disk
                 print "Updating registry with new modified datetime of %s." % modified
                 registry_file[provider]['modified'] = modified
-                registry.save_registry("/glharvest/registry.yml", registry_file)
-            else:
-                print "datadump has not been updated since the last visit. Doing nothing."
 
 
 def export():
     """Export the repository as Turtle."""
+    JOB_NAME = "EXPORT"
+    logging.info("[%s] Job started.", JOB_NAME)
 
     s = Store(SESAME_HOST, SESAME_PORT)
     r = Repository(s, SESAME_REPOSITORY)
 
-    with open('/www/export.ttl', 'wb') as f:
+    # Establish the location of the export file
+    # The else case will run when not running in a container
+    if os.path.isdir('/www/'):
+        export_filepath = '/www/exprort.ttl'
+    else:
+        export_filepath = 'exprort.ttl'
+
+    with open(export_filepath, 'wb') as f:
         exported_text = r.export()
         f.write(exported_text.encode('utf-8'))
 
 
 def status():
     """Gets the status of the system and prints to stdout."""
-    print "Getting the status of the system."
+    JOB_NAME = "STATUS"
+    logging.info("[%s] Job started.", JOB_NAME)
 
     s = Store(SESAME_HOST, SESAME_PORT)
     r = Repository(s, SESAME_REPOSITORY)
 
-    print "Repository size is %d" % r.size()
+    logging.info("[%s] Repository size is '%d'.", JOB_NAME, r.size())
